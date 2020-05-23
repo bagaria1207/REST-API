1177b490c0ce5698cc2c8129729b85525e1eeb7b
Apurva <bagaria.apurva@gmail.com>
2020-05-23 11:14:47 +0530

    completed

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const userRouter = require("./api/users/user.router");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/users",userRouter);
+
+
+app.listen(process.env.APP_PORT, ()=>{
+    console.log("Server up and running on port", process.env.APP_PORT);
+})
